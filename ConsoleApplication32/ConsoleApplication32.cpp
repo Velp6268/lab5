845a31699ad6118d898759303444a2be64fa68a5
user <user@C1011>
2021-12-13 14:41:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication32/ConsoleApplication32.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, n;
+    cout << "Введите значение: " << endl;
+    cout << "n= ";
+    cin >>  n;
+   
+
+    cout<< "an= " << (1 / ((3 * n - 2) * (3 * n + 1)));
+
+    system("pause");
+    return 0;
+}
